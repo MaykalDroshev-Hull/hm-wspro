0635693891d6ed3a2f43cd07aae7ea960d6d52b0
Maykal Droshev <72551672+MaykalDroshev-Hull@users.noreply.github.com>
2025-08-14 22:44:10 +0100

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { TranslationProvider } from "@/contexts/TranslationContext"
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
@@ -25,6 +26,7 @@ export default function RootLayout({
             {children}
           </ThemeProvider>
         </TranslationProvider>
+        <Analytics />
       </body>
     </html>
   )
